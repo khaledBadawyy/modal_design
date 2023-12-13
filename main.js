2cccf9183de1e799e6037de070de4188ef2f4e33
khaledBadawyy <khaledbadawy315@gmail.com>
2023-12-13 10:54:19 +0200

    Added new files

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-import { Internet_Connection } from "./Script.js";
+class Internet_Connection {
+  constructor() {
+    this.modal = document.getElementById("modal");
+    this.modalMessage = document.getElementById("modal-message");
+    this.checkConnection();
+    window.addEventListener("online", () => this.checkConnection());
+    window.addEventListener("offline", () => this.checkConnection());
+  }
+
+  checkConnection() {
+    if (navigator.onLine) {
+      if (this.modal.style.display !== "none") {
+        this.modal.style.display = "none";
+      }
+    } else {
+      if (this.modal.style.display === "none") {
+        this.modal.style.display = "block";
+      }
+    }
+  }
+}
 
 class UI_Widget {
   constructor() {
@@ -16,3 +36,4 @@ class UI_Widget {
 
 const internetConnection = new Internet_Connection();
 const uiWidget = new UI_Widget();
+console.log(internetConnection)
